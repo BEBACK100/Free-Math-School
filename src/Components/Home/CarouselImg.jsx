943393d2748e0247daf71e4d32b1287cc1b5c685
DELL-PC <beback_bain@yahoo.com>
2023-11-01 14:46:20 +0600

    home page decoration and carousel img

---
+++ src/Components/Home/CarouselImg.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import img1 from '../../assets/Carouselimg/sch1.jpg'
+import img2 from '../../assets/Carouselimg/sch2.jpeg'
+import img3 from '../../assets/Carouselimg/sch3.jpeg'
+import img4 from '../../assets/Carouselimg/sch4.jpeg'
+
+
+
+
+const CarouselImg = () => {
+    return (
+        <div className="carousel w-full">
+            <h1>Carousel image</h1>
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src={img1} className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">❮</a> 
+            <a href="#slide2" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+        <div id="slide2" className="carousel-item relative w-full">
+          <img src={img2} className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">❮</a> 
+            <a href="#slide3" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+        <div id="slide3" className="carousel-item relative w-full">
+          <img src={img3} className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">❮</a> 
+            <a href="#slide4" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+        <div id="slide4" className="carousel-item relative w-full">
+          <img src={img4} className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">❮</a> 
+            <a href="#slide1" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default CarouselImg;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
-import Header from '../Header/Header';
+
+import CarouselImg from './CarouselImg';
+import Marquee from 'react-fast-marquee';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div>
-          
+          <Marquee behavior="pusheOnClick" className='text-blue-500' speed="50" direction="left">
+            <Link>
+            <h1>Rpid News</h1>
+            </Link>
+          </Marquee>
             <h1>This is Home</h1>
+            <CarouselImg></CarouselImg>
+
+            <div className='bg-blue-300 p-5 my-8 w-1/2 justify-center mx-auto'>
+                <h1>Sharjah Indian School is a Senior
+                     Secondary School, Owned and Managed
+                      by Indian Association Sharjah. Established
+                       in 1979 with 280 pupils and 13 teachers, 
+                       the school stands as a landmark in the
+                        annals of the Indo- UAE friendship. 
+                        Today after 43 years, the school has a 
+                        strength of 13,937 students and 588 teachers
+                         with 8300 Students and 349 Teachers at 
+                         Ghubaiba Branch . The school is approved and
+                          licensed by the Sharjah Private Education 
+                          Authority , UAE and is affiliated to Central 
+                          Board of Secondary Education, New Delhi ,
+                           India. The school which stands on a plot of 
+                           land most graciously donated by H.H Sheikh
+                            Sultan Bin Mohammed Al Qassimi, Member of
+                             the Supreme Council and Ruler of Sharjah,
+                              has a purpose built and magnificent 
+                              infrastructure.</h1>
+            </div>
         </div>
     );
 };
