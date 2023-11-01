5dab89cc6ca59a5c6924a10e99d500f177513c7d
DELL-PC <beback_bain@yahoo.com>
2023-11-01 13:20:20 +0600

    Header part

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { Link, NavLink } from 'react-router-dom';
 const Header = () => {
     return (
         <div className="navbar bg-blue-500">
-        <div className="navbar-start">
+        <div className="navbar flex-col">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -14,73 +14,104 @@ const Header = () => {
               <li>
                 <a>Sumary</a>
                 <ul className="p-2">
-                  <li><NavLink to="/contact" >Contact</NavLink ></li>
-                  <li><NavLink to="/product" >Product</NavLink ></li>
-                  <li><NavLink to="/about" >About Us</NavLink  ></li>
+                  <li><NavLink to="/academic" >Academic</NavLink ></li>
+                  <li><NavLink to="/admission" >Admission</NavLink ></li>
+                  <li><NavLink to="/aboutUs" >About Us</NavLink  ></li>
                 </ul>
               </li>
               <li>
                 <a>Sumary</a>
                 <ul className="p-2">
-                  <li><NavLink to="/contact" >Contact</NavLink ></li>
-                  <li><NavLink to="/product" >Product</NavLink ></li>
-                  <li><NavLink to="/about" >About Us</NavLink  ></li>
+                <li><NavLink to="/academic" >Academic</NavLink ></li>
+                  <li><NavLink to="/admission" >Admission</NavLink ></li>
+                  <li><NavLink to="/aboutUs" >About Us</NavLink  ></li>
                 </ul>
               </li>
               <li>
                 <a>Sumary</a>
                 <ul className="p-2">
-                  <li><NavLink to="/contact" >Contact</NavLink ></li>
-                  <li><NavLink to="/product" >Product</NavLink ></li>
-                  <li><NavLink to="/about" >About Us</NavLink  ></li>
+                <li><NavLink to="/academic" >Academic</NavLink ></li>
+                  <li><NavLink to="/admission" >Admission</NavLink ></li>
+                  <li><NavLink to="/aboutUs" >About Us</NavLink  ></li>
                 </ul>
               </li>
               <li><a>Item 3</a></li>
             </ul>
           </div>
-          <a className="btn btn-ghost normal-case text-xl">MB online Cloth</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1 z-[1] " viewBox="0 0 24 24">
+          {/* <a className="btn btn-ghost normal-case text-xl">MB online Cloth</a> */}
+         <div>
+         <Link to="/" className="btn btn-ghost normal-case text-xl">MB online Cloth</Link>
+         <NavLink to="/academic" >Academic</NavLink >
+               <NavLink to="/admission" >Admission</NavLink >
+         </div>
+
+        <div className=" hidden lg:flex bg-red-300 justify-center rounded-lg ">
+          <ul className="menu menu-horizontal px-1 z-[1]  " viewBox="0 0 24 24">
             <li><NavLink to='/'>Home</NavLink ></li>
-            <li><NavLink  to='/about'>About Us</NavLink ></li>
-            <li><NavLink  to='/contact'>Contact</NavLink ></li>
             
-            <li><Link  to='/product'>Product</Link ></li>
+                  
             
             <li tabIndex={0}>
               <details>
-                <summary>Summary</summary>
+                <summary><NavLink  to='/aboutUs' viewBox="0 0 24 24">About Us</NavLink ></summary>
                 <ul className="p-2">
-                  <li><Link to ="/about">About Us</Link></li>
+                  <li><Link to ="/aboutUs">About Us</Link></li>
                   <li><Link to="/contact">Contact</Link></li>
                 </ul>
               </details>
             </li>
             <li tabIndex={0}>
               <details>
-                <summary>Summary</summary>
+                <summary><NavLink to="/admission" >Admission</NavLink ></summary>
                 <ul className="p-2">
-                  <li><Link to ="/about">About Us</Link></li>
+                  <li><Link to ="/aboutUs">About Us</Link></li>
                   <li><Link to="/contact">Contact</Link></li>
                 </ul>
               </details>
             </li>
             <li tabIndex={0}>
               <details>
-                <summary>Summary</summary>
+                <summary><NavLink to="/academic" >Academic</NavLink ></summary>
                 <ul className="p-2">
-                  <li><Link to ="/about">About Us</Link></li>
+                  <li><Link to ="/aboutUs">About Us</Link></li>
                   <li><Link to="/contact">Contact</Link></li>
                 </ul>
               </details>
             </li>
-          
-            <li><a></a></li>
+            <li tabIndex={0}>
+              <details>
+                <summary>Gallery</summary>
+                <ul className="p-2">
+                  <li><Link to ="/aboutUs">About Us</Link></li>
+                  <li><Link to="/contact">Contact</Link></li>
+                </ul>
+              </details>
+            </li>
+            <li tabIndex={0}>
+              <details>
+                <summary>General</summary>
+                <ul className="p-2">
+                  <li><Link to ="/aboutUs">About Us</Link></li>
+                  <li><Link to="/contact">Contact</Link></li>
+                </ul>
+              </details>
+            </li>
+            <li tabIndex={0}>
+              <details>
+                <summary>Circulars</summary>
+                <ul className="p-2">
+                  <li><Link to ="/aboutUs">About Us</Link></li>
+                  <li><Link to="/contact">Contact</Link></li>
+                </ul>
+              </details>
+            </li>
+        
           </ul>
         </div>
+        </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <Link to='login' className="btn mr-3">Online Payment</Link>
+          <Link to='login' className="btn">Login</Link>
         </div>
       </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,13 @@ import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './Components/Home/Home.jsx'
 import MainLayout from './Components/Layout/MainLayout.jsx'
+import AboutUs from './Components/About-us/AboutUs.jsx'
+import Academic from './Components/Academic/Academic.jsx'
+import Login from './Components/SignIn/Login.jsx'
+import Gallery from './Components/Gallery/Gallery.jsx'
+import General from './Components/General/General.jsx'
+import Circulars from './Components/Circular/Circulars.jsx'
+import Admission from './Components/Admission/Admission.jsx'
 
 const router=createBrowserRouter([
 {
@@ -14,7 +21,39 @@ const router=createBrowserRouter([
     {
       path:'/',
       element:<Home></Home>
-    }
+    },
+    {
+      path:'/aboutUs',
+      element:<AboutUs></AboutUs>
+    },
+    {
+      path:'/academic',
+      element:<Academic></Academic>
+    },
+    {
+      path:'/admission',
+      element:<Admission></Admission>
+    },
+    {
+      path:'/gallery',
+      element:<Gallery></Gallery>
+    },
+    {
+      path:'/general',
+      element:<General></General>
+    },
+    {
+      path:'/circular',
+      element:<Circulars></Circulars>
+    },
+    {
+      path:'/signup',
+      element:<Login></Login>
+    },
+    {
+      path:'/login',
+      element:<Login></Login>
+    },
   ]
 }
 ])
